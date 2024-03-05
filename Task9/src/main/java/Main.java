2dd6889214fd4c6caa61e149383491ba11f1a09d
Catalyst-42 <catalyst42c@gmail.com>
2024-03-05 14:53:51 +0300

    9

---
+++ Task9/src/main/java/Main.java
@@ -0,0 +1,9 @@
+// package Task9.src.main.java;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\033[31m");
+        System.out.println("--> It works! <--");
+        System.out.println("\033[0m");
+    }
+}
